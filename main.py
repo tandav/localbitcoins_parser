370c804d9041241d727f687ac0c2a7673002e962
Alexander Rodionov <alx1dr@gmail.com>
2020-03-30 02:26:17 +0300

    _

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 TODO:
 save all, plot only last 48 hours
 maybe use only banks in rubles (check different options)
-fixstddev
+fix stddev
+track each user
+maybe plot only +- 3std (drop outliers)
 '''
 
 
@@ -22,60 +24,73 @@ import dateutil
 
 
 N_PRICES = 50
-DB_PATH  = Path('prices.pkl')
+DB_PATH  = Path('db.pkl')
 API_URL  = 'https://localbitcoins.net/ru/buy-bitcoins-online/rub'
 MSK = dateutil.tz.gettz('Europe/Moscow')
 DATE_FORMAT = DateFormatter('%H:%M', tz=MSK)
 PRICE_FORMAT = StrMethodFormatter('{x:,.0f}')
+YLIM = 20 # stddev
+
 
 if len(sys.argv) == 2 and sys.argv[1] == 'init':
     assert not DB_PATH.exists()
-    d = pd.DataFrame(data=[], columns=range(N_PRICES))
-    d.index = pd.to_datetime(d.index)
-    d.to_pickle(DB_PATH)
+    pd.DataFrame().to_pickle(DB_PATH)
     print('init done')
     sys.exit(0)
 
 
-def get_prices():
+def get_ads():
     _ = API_URL
     _ = requests.get(_)
     _ = _.text
     _ = BeautifulSoup(_, features='html.parser')
-    _ = _.find_all('td', class_='column-price')
-    _ = map(lambda x: x.text            , _)
-    _ = map(lambda x: x.strip()         , _)
-    _ = map(lambda x: x.replace(',', ''), _)
-    _ = map(lambda x: x[:-4]            , _)
-    _ = map(float                       , _)
-    _ = sorted(_)
-    assert len(_) == N_PRICES
+    _ = _.find_all('tr', class_='clickable')
     return _
 
 
 while True:
-    db = pd.read_pickle(DB_PATH)
+    d = pd.read_pickle(DB_PATH)
     now = datetime.datetime.now(tz=MSK)
-    print(now)
-    db.loc[now] = get_prices()
-    db.to_pickle(DB_PATH)
 
-    mean = db.mean(axis=1)
+    for row in get_ads():
+        # user  = row.find('td', class_='column-user').find('a').attrs['href'].split('/')[-2]
+        ad    = row.find('td', class_='column-button').find('a').attrs['href'].split('/')[2]
+        price = float(row.find('td', class_='column-price').text.strip()[:-4].replace(',', ''))
+        d.loc[now, ad] = price
+    print(now, d.shape)
+    d.to_pickle(DB_PATH)
 
-    ax = db.plot(figsize=(16, 10), legend=False, marker=',', linestyle='-', linewidth=0.9)
+    mean = d.mean(axis=1)
+    std  = d.std(axis=1)
+
+    mean_global = d.mean().mean()
+    std_global = d.std().mean()
+    max_global = d.max().max()
+    min_global = d.min().min()
+
+    y_low  = max(mean_global - YLIM * std_global, min_global)
+    y_high = min(mean_global + YLIM * std_global, max_global)
+
+    # ax = d.interpolate(limit_area='inside').plot(figsize=(16, 10), legend=False, marker=None, linestyle='-', linewidth=0.9)
+    ax = d.plot(figsize=(16, 10), legend=False, marker='.', markersize=1, linestyle='none')
+
     ax.xaxis.set_major_formatter(DATE_FORMAT)
     ax.yaxis.set_major_formatter(PRICE_FORMAT)
-    mean.plot(color='black', marker=',', linestyle='-', linewidth=3)
+    mean.plot(color='black', marker=None, linestyle='-', linewidth=3)
+    plt.fill_between(std.index, mean - std, mean + std, color='grey', alpha=.1, zorder=-1)
+    plt.ylim(y_low, y_high)
+
     plt.grid(lw=0.3)
-    plt.title(f'{API_URL}   (top 50 offers, sorted)')
-    plt.xlabel(f'updates every minute, last update: {now:%Y %b %d %H:%M:%S} MSK')
+    plt.title(f'{API_URL}   | top 50 offers from 1st page')
+    plt.xlabel(f'last update:    {now:%Y %b %d %H:%M:%S} MSK')
     plt.ylabel('1 BTC price in RUB')
     plt.tight_layout()
     ax.figure.savefig('prices.png')
     fig = ax.get_figure()
     plt.close(fig)
 
-    time.sleep(55)
+    # time.sleep(55)
+    time.sleep(5)
 
 
 
